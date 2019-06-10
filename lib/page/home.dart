7ecf43e5dd797096bc965b8359e65764175572c9
wangtao02 <wangt02@zendaimoney.com>
2019-06-10 16:34:43 +0800

    dio

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,86 +1,58 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Post> fetchPost() async {
+  final response =
+      await http.get('http://18.10.1.115:4000/get/alluser');
+  final responseJson = json.decode(response.body);
+
+  return new Post.fromJson(responseJson);
+}
+
+class Post {
+  final String message;
+  // final List users;
+  Post({this.message});
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return new Post(
+      message: json['message'],
+    );
+  }
+}
+
+// void main() => runApp(new MyApp());
 
 class HomeScreen extends StatelessWidget {
-  final List<String> entries = <String>['A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C','A', 'B', 'C','A', 'B', 'C','A', 'B', 'C'];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("微信"),
+    return new MaterialApp(
+      title: '',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body:
-        ListView.builder(
-            padding: const EdgeInsets.all(8.0),
-            itemCount: entries.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                padding: EdgeInsets.all(5),
-                height: 50,
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      flex: 7,
-                      child: Image(
-                            image: NetworkImage(
-                            "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3315110696,2025759830&fm=27&gp=0.jpg"),
-                            width: 50.0,
-                          ),
-                    ),
-                    Expanded(
-                      flex:1,
-                      child:Padding(
-                        padding: EdgeInsets.only(left: 5),
-                      )
-                    ),
-                    Expanded(
-                      flex: 35,
-                      child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text('秦王嬴政'),
-                              Container(
-                                child: FittedBox(
-                                  fit: BoxFit.fitWidth, 
-                                  child: Text('小赵，明天我要去灭楚庄王那孙子，你别插手....', style: TextStyle(
-                                    fontSize: 12,
-                                    color: Colors.black26
-                                  )),
-                                ),
-                              ),
-                              new Padding(
-                                padding: EdgeInsets.only(bottom: 2),
-                              ),
-                              new Divider(
-                                height: 5,
-                                color: Colors.black26,
-                              )
-                            ],
-                          ),
-                    ),
-                    Expanded(
-                      flex: 5,
-                      child: FittedBox(
-                        child: 
-                          Column(
-                            children: <Widget>[
-                              Text('凌晨 0:09', style: TextStyle(
-                                fontSize: 12,
-                                color: Colors.black26
-                              ),),
-                              Icon(Icons.accessible,color: Colors.black26,),
-                              new Divider( // 颜色没显示出来
-                                height: 5,
-                                color: Colors.black26,
-                              )
-                            ],
-                          )
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            }
-        )
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('微信'),
+        ),
+        body: new Center(
+          child: new FutureBuilder<Post>(
+            future: fetchPost(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return new Text(snapshot.data.message);
+              } else if (snapshot.hasError) {
+                return new Text("${snapshot.error}");
+              }
+
+              // By default, show a loading spinner
+              return new CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/page/userList.dart
+++ lib/page/userList.dart
@@ -1,12 +1,31 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+// import '../utils/HttpRequest.dart';
 
-// void main() => runApp(new UserListScreen());
+// _handleGetShelf () async {
+//   var result = await HttpRequest.request(
+//     '/get/alluser', 
+//     method: HttpRequest.GET,
+//     data: {}
+//   );
+//   print(result);
+// }
+
+void getHttp() async {
+  try {
+    Response response = await Dio().get("http://18.10.1.115:4000/get/alluser");
+    print(response);
+  } catch (e) {
+    print(e);
+  }
+}
 
 class UserListScreen extends StatelessWidget {
   final List<String> entries = <String>['A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C','A', 'B', 'C','A', 'B', 'C','A', 'B', 'C'];
   final List<int> colorCodes = <int>[600, 500, 100, 600, 500, 100,600, 500, 100, 600, 500, 100,600, 500, 100,600, 500, 100,];
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return Scaffold(
       appBar: AppBar(
         title: Text("通讯录"),
